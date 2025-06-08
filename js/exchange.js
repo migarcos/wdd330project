bcef857bd61f7ab32defe1a1f3c26263336d98aa
migarcos <migark@hotmail.com>
2025-06-08 03:11:33 -0500

    upd

---
+++ js/exchange.js
@@ -0,0 +1,6 @@
+const API_KEY = "cur_live_ISPKqJqmEs3r0AgsNtMTKhrXqhy0XlaEnZ00S11X";
+const URL = "https://api.currencyapi.com/v3/latest?apikey=";
+
+export async function getExchangeRates(base, currencies) {
+    
+}
---
+++ js/utils.mjs
@@ -0,0 +1,27 @@
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  // Load the header and footer templates in from the partials using the loadTemplate.
+  const headerTemplate = await loadTemplate("../public/header.html");
+  const navTemplate = await loadTemplate("../public/nav.html");
+  const footerTemplate = await loadTemplate("../public/footer.html");
+  //Grab the header and footer placeholder elements out of the DOM
+  const headerElement = document.querySelector("#header");
+  const navElement = document.querySelector("#nav");
+  const footerElement = document.querySelector("#footer");
+  // Render the header and footer using renderWithTemplate
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(navTemplate, navElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
