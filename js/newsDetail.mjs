ee4216da1cee96ba178ea9fbb916c9ddbb82865a
migarcos <migark@hotmail.com>
2025-10-09 20:05:08 -0500

    Individual news class creation success

---
+++ js/newsDetail.mjs
@@ -0,0 +1,30 @@
+export default class NewsDetail {
+
+    constructor(media, order) {
+        this.media = media;
+        this.order = order;
+    }
+
+    ini() {
+        const news = JSON.parse(localStorage.getItem(this.media));
+        console.log(news[this.order][2]);
+        this.displayNew(news[this.order]);
+    }
+
+    displayNew(element) {
+        document.querySelector("#newscontent p").style.display = "none";
+        document.querySelector("#newscontent h2").textContent = element[0];
+        document.querySelector("#newscontent h1").textContent = element[1];
+        document.querySelector("#newscontent img").src = element[2];
+        document.querySelector("#newscontent img").alt = element[1];
+
+        if (this.media === "guardianHeaders") {    
+            
+            document.querySelector("#newscontent article").innerHTML = element[3];
+        } else {
+            document.querySelector("#newscontent img").src = element[4];
+            document.querySelector("#newscontent p").style.display = "block";
+            document.querySelector("#newscontent p").textContent = element[3];            
+        }
+    }
+}
--- js/newsIndividual.js
+++ js/newsIndividual.js
@@ -1,21 +1,27 @@
+import NewsDetail from "./newsDetail.mjs";
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
 let media = getParam("media");
 let order = parseInt(getParam("order"));
-console.log(media);
-const news = JSON.parse(localStorage.getItem(media)); // [0]
 
-console.log(news);
-document.querySelector("#newscontent h2").textContent = news[order][0];
-document.querySelector("#newscontent h1").textContent = news[order][1];
-document.querySelector("#newscontent img").src = news[order][2];
-document.querySelector("#newscontent img").alt = news[order][1];
+const content = new NewsDetail(media, order);
 
-if (media === "guardianHeaders") {
-    document.querySelector("#newscontent article").innerHTML += news[order][3];
-} else {
-    document.querySelector("#newscontent img").src = news[order][4];
-    document.querySelector("#newscontent p").textContent = news[order][3];
-}
+content.ini();
+
+// console.log(media);
+// const news = JSON.parse(localStorage.getItem(media)); // [0]
+
+// // console.log(news);
+// document.querySelector("#newscontent h2").textContent = news[order][0];
+// document.querySelector("#newscontent h1").textContent = news[order][1];
+// document.querySelector("#newscontent img").src = news[order][2];
+// document.querySelector("#newscontent img").alt = news[order][1];
+
+// if (media === "guardianHeaders") {
+//     document.querySelector("#newscontent article").innerHTML += news[order][3];
+// } else {
+//     document.querySelector("#newscontent img").src = news[order][4];
+//     document.querySelector("#newscontent p").textContent = news[order][3];
+// }
